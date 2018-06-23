8a106b51ba53d5ae0dd858a284e57ef1c7690649
Calvin Rai <raicalvin@gmail.com>
2018-06-23 09:54:58 -0700

    Updated custom ArrayAdapter constructor.

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -1,6 +1,20 @@
 package com.example.android.quakereport;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.widget.ArrayAdapter;
 
-public class EarthquakeAdapter extends ArrayAdapter {
+import java.util.ArrayList;
+
+public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
+
+    /**
+     * Constructor
+     *
+     * @param context  The current context.
+     * @param earthquakes The ArrayList containing all Earthquake objects
+     */
+    public EarthquakeAdapter(@NonNull Context context, ArrayList<Earthquake> earthquakes) {
+        super(context, 0, earthquakes);
+    }
 }
