ba016b1be523aa9e592a2b5babdcfb500194de74
Calvin Rai <raicalvin@gmail.com>
2018-06-23 10:11:53 -0700

    Updated getView method to inflate list item if convertView == null.

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -2,11 +2,18 @@ package com.example.android.quakereport;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 
 public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
+
+    private Context mContext;
+    private ArrayList<Earthquake> mEarthquakesList;
 
     /**
      * Constructor
@@ -16,5 +23,23 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
      */
     public EarthquakeAdapter(@NonNull Context context, ArrayList<Earthquake> earthquakes) {
         super(context, 0, earthquakes);
+        mContext = context;
+        mEarthquakesList = earthquakes;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param position is the position of the current Earthquake item
+     * @param convertView View to be reused in ListView
+     * @param parent maybe the ListView in the earthquake_activity?
+     */
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItem = convertView;
+        if (convertView == null) {
+            listItem = LayoutInflater.from(mContext).inflate(R.layout.eq_list_item, parent, false);
+        }
     }
 }
