a0b9b6f9e46234054c497a70eaf45f4af7d2f137
Calvin Rai <raicalvin@gmail.com>
2018-06-23 10:22:39 -0700

    Updated getView to inflate TextViews with proper placeholder data from EQ object.

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -37,9 +38,22 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
         View listItem = convertView;
+
         if (convertView == null) {
             listItem = LayoutInflater.from(mContext).inflate(R.layout.eq_list_item, parent, false);
         }
+
+        Earthquake currentEQ = mEarthquakesList.get(position);
+
+        TextView magnitudeTextView = (TextView) listItem.findViewById(R.id.magnitude_text_view);
+        TextView locationTextView = (TextView) listItem.findViewById(R.id.location_text_view);
+        TextView dateTextView = (TextView) listItem.findViewById(R.id.date_text_view);
+
+        magnitudeTextView.setText(String.valueOf(currentEQ.getmMagnitude()));
+        locationTextView.setText(currentEQ.getmLocation());
+        dateTextView.setText(currentEQ.getmDate());
+
     }
 }
