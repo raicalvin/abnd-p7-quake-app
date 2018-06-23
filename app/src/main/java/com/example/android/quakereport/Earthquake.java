094490e184ae74ef06bc7360a3ca0c41fa07b8a0
Calvin Rai <raicalvin@gmail.com>
2018-06-23 09:28:25 -0700

    Declared empty private member variables to hold magnitude, location, and date.

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -2,7 +2,11 @@ package com.example.android.quakereport;
 
 public class Earthquake {
 
+    private double mMagnitude; private String location; private String date;
+
     public Earthquake(double magnitude, String location, String date) {
+
+
 
     }
 
