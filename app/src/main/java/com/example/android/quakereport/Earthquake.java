e24b67ec8afef235c5fa2c6fbf19c35cc9ad959a
Calvin Rai <raicalvin@gmail.com>
2018-06-23 09:29:30 -0700

    Assigned private member variables constructor inputs.

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -2,11 +2,11 @@ package com.example.android.quakereport;
 
 public class Earthquake {
 
-    private double mMagnitude; private String location; private String date;
+    private double mMagnitude; private String mLocation; private String mDate;
 
     public Earthquake(double magnitude, String location, String date) {
 
-
+        mMagnitude = magnitude; mLocation = location; mDate = date;
 
     }
 
